312e382de8be64cb7d9b0d98fbc0cdf89ee5949c
Lukas Harzenetter <lharzenetter@gmx.de>
2021-09-21 15:19:35 +0200

    Fix Docker (#11)
    * update DockerEngine Interfaces
    
    * fix namespaces
    
    * fixes
    
    * fix log
    
    * fix docker IA
    
    * update logs

--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/DockerEngine_DockerEngine-Interface-w1/source/src/main/java/org/opentosca/NodeTypes/AbstractIAService.java
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/DockerEngine_DockerEngine-Interface-w1/source/src/main/java/org/opentosca/NodeTypes/AbstractIAService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import javax.annotation.Resource;
-import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.xml.ws.WebServiceContext;
@@ -18,78 +17,78 @@ import org.w3c.dom.Node;
 
 public abstract class AbstractIAService {
 
-	@Resource
-	private WebServiceContext context;
+    @Resource
+    private WebServiceContext context;
 
-	private Client client;
+    public AbstractIAService() {
+    }
 
-	public AbstractIAService() {
-		this.client = ClientBuilder.newClient();
-	}
+    protected void sendResponse(HashMap<String, String> returnParameters) {
 
-	protected void sendResponse(HashMap<String, String> returnParameters) {
+        // Extract message
+        WrappedMessageContext wrappedContext = (WrappedMessageContext) context.getMessageContext();
+        Message message = wrappedContext.getWrappedMessage();
 
-		// Extract message
-		WrappedMessageContext wrappedContext = (WrappedMessageContext) context.getMessageContext();
-		Message message = wrappedContext.getWrappedMessage();
+        // Extract headers from message
+        List<Header> headers = CastUtils.cast((List<?>) message.get(Header.HEADER_LIST));
 
-		// Extract headers from message
-		List<Header> headers = CastUtils.cast((List<?>) message.get(Header.HEADER_LIST));
+        // Find ReplyTo and MessageID SOAP Header
+        String replyTo = null;
+        String messageID = null;
+        for (Header iter : headers) {
 
-		// Find ReplyTo and MessageID SOAP Header
-		String replyTo = null;
-		String messageID = null;
-		for (Header iter : headers) {
+            Object headerObject = iter.getObject();
 
-			Object headerObject = iter.getObject();
+            // Unmarshall to org.w3c.dom.Node
+            if (headerObject instanceof Node) {
+                Node node = (Node) headerObject;
+                String localPart = iter.getName().getLocalPart();
+                String content = node.getTextContent();
 
-			// Unmarshall to org.w3c.dom.Node
-			if (headerObject instanceof Node) {
-				Node node = (Node) headerObject;
-				String localPart = iter.getName().getLocalPart();
-				String content = node.getTextContent();
+                // Extract ReplyTo Header value
+                if ("ReplyTo".equals(localPart)) {
+                    replyTo = content;
+                }
 
-				// Extract ReplyTo Header value
-				if ("ReplyTo".equals(localPart)) {
-					replyTo = content;
-				}
+                // Extract MessageID Header value
+                if ("MessageID".equals(localPart)) {
+                    messageID = content;
+                }
+            }
+        }
 
-				// Extract MessageID Header value
-				if ("MessageID".equals(localPart)) {
-					messageID = content;
-				}
-			}
-		}
+        // Create asynchronous SOAP Response Message
+        StringBuilder builder = new StringBuilder();
 
-		// Create asynchronous SOAP Response Message
-		StringBuilder builder = new StringBuilder();
+        builder.append(
+                "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:sch='http://siserver.org/schema'>");
+        builder.append("   <soapenv:Header/>");
+        builder.append("   <soapenv:Body>");
+        builder.append("      <sch:invokeResponse>");
+        builder.append("         <sch:MessageID>").append(messageID).append("</sch:MessageID>");
 
-		builder.append(
-				"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:sch='http://siserver.org/schema'>");
-		builder.append("   <soapenv:Header/>");
-		builder.append("   <soapenv:Body>");
-		builder.append("      <sch:invokeResponse>");
-		builder.append("         <sch:MessageID>" + messageID + "</sch:MessageID>");
+        // Insert return parameters into asynchronous SOAP Response Message
+        for (Entry<String, String> paramIter : returnParameters.entrySet()) {
 
-		// Insert return parameters into asynchronous SOAP Response Message
-		for (Entry<String, String> paramIter : returnParameters.entrySet()) {
+            String key = paramIter.getKey();
+            String value = paramIter.getValue();
 
-			String key = paramIter.getKey();
-			String value = paramIter.getValue();
+            builder.append("         <").append(key).append(">").append(value).append("</").append(key).append(">");
+        }
 
-			builder.append("         <" + key + ">" + value + "</" + key + ">");
-		}
+        builder.append("       </sch:invokeResponse>");
+        builder.append("    </soapenv:Body>");
+        builder.append("</soapenv:Envelope>");
 
-		builder.append("      </sch:invokeResponse>");
-		builder.append("	</soapenv:Body>");
-		builder.append("</soapenv:Envelope>");
-
-		// Send SOAP Response Message back to requester
-		if (replyTo == null) {
-			System.err.println(
-					"No 'ReplyTo' header found!\nTherefore, reply message is printed here:\n" + builder.toString());
-		} else {
-			this.client.target(replyTo).request().post(Entity.xml(builder.toString()));
-		}
-	}
+        // Send SOAP Response Message back to requester
+        if (replyTo == null) {
+            System.err.println(
+                    "No 'ReplyTo' header found!\nTherefore, reply message is printed here:\n" + builder.toString());
+        } else {
+            ClientBuilder.newClient().target(replyTo)
+                    .request()
+                    .post(Entity.xml(builder.toString()))
+                    .close();
+        }
+    }
 }
