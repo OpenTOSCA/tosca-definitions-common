52e396741ff3079836b667e6d309005c8e9b4191
Lukas Harzenetter <lharzenetter@gmx.de>
2021-09-28 23:47:42 +0200

    fix gunicorn install

--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/Gunicorn_install_20.1.0-install-w1/files/install.sh
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/Gunicorn_install_20.1.0-install-w1/files/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python3 -m pip install click==7.1.2 # otherwise conflict with the plugin runner
+python3 -m pip install click-didyoumean==0.0.3 # https://pypi.org/project/click-didyoumean/#history they actually broke everthing in exactly one hour we were testing
 python3 -m pip install gunicorn==20.1.0
