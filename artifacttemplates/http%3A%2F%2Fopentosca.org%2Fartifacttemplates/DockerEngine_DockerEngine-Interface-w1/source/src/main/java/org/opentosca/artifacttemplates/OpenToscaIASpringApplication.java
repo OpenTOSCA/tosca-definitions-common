c6cc51f4658474fa3eb8acddf22090c396ba51ee
Lukas Harzenetter <lharzenetter@gmx.de>
2022-03-10 09:27:05 +0100

    add test for DockerEngine IA

--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/DockerEngine_DockerEngine-Interface-w1/source/src/main/java/org/opentosca/artifacttemplates/OpenToscaIASpringApplication.java
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/DockerEngine_DockerEngine-Interface-w1/source/src/main/java/org/opentosca/artifacttemplates/OpenToscaIASpringApplication.java
@@ -1,13 +1,13 @@
-package org.opentosca.artifacttemplates.dockerengine;
+package org.opentosca.artifacttemplates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerEngineInterfaceDockerEngineApplication extends SpringBootServletInitializer {
+public class OpenToscaIASpringApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(DockerEngineInterfaceDockerEngineApplication.class, args);
+        SpringApplication.run(OpenToscaIASpringApplication.class, args);
     }
 }
