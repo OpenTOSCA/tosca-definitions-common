3beed15d4e52e1019bcf67d06b704113ee5ad21d
Lukas Harzenetter <lharzenetter@gmx.de>
2022-08-03 11:45:59 +0200

    add new Type and Implentations

---
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/MySQL-DBMS_8.0-Start-w1/files/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo /etc/init.d/mysql start
+
+# complete this installation using the configuration script according to https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-22-04
+
+sleep 5
--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/SpringWebApp_Start-w1/files/start.sh
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/SpringWebApp_Start-w1/files/start.sh
@@ -2,8 +2,11 @@
 cd /var/www/java/${AppName}
 
 nohup java -jar ${AppName}.jar --server.port=$Port > /var/log/${AppName}.log 2>&1 &
+echo $! > ${AppName}.pid
+
+ln -s /var/log/${AppName}.log
 
 # Inhibit some race condition
 sleep 5
 
-echo "Started Spring Web Application $AppName"
+echo "Started ${AppName} with process ID: $(cat ${AppName}.pid)"
