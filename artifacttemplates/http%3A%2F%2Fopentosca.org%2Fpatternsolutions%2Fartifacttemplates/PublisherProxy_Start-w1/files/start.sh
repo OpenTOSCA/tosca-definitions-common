84f3b0b1ba15d311d329ea1107b294b0a463eeed
Lukas Harzenetter <lharzenetter@gmx.de>
2023-01-19 09:37:36 +0100

    make port configurable for publisherProxy

--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fpatternsolutions%2Fartifacttemplates/PublisherProxy_Start-w1/files/start.sh
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fpatternsolutions%2Fartifacttemplates/PublisherProxy_Start-w1/files/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 HOMEDIR=$HOME/pythonapp
 cd $HOMEDIR/publisherProxy
-nohup python3 main.py 9993 "$HOMEDIR/driver-manager.yml" > log.log 2>&1 &
+
+nohup python3 main.py $Port "$HOMEDIR/driver-manager.yml" > log.log 2>&1 &
 echo $! > publisherProxy.pid
+
 sleep 5
