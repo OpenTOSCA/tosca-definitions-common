d1af26b5a8ec2c240adb2d2c31f43bdec659463a
Lukas Harzenetter <lharzenetter@gmx.de>
2023-03-29 10:01:24 +0200

    fix mysql install

--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/MySQL-DBMS_8.0-Install-w1/files/install.sh
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/MySQL-DBMS_8.0-Install-w1/files/install.sh
@@ -2,10 +2,13 @@
 sudo sh -c "echo '127.0.0.1' $(hostname) >> /etc/hosts";
 sudo apt-get update -qq;
 
-# disables setting the root password with gui, root password etc. will be set in the configure.sh
-export DEBIAN_FRONTEND=noninteractive;
+echo "Europe/Berlin" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 
-sudo apt-get install -y mysql-server-8.0
+# disables setting the root password with gui, root password etc. will be set in the configure.sh
+export DEBIAN_FRONTEND="noninteractive" TZ="UTC"
+
+sudo -E apt-get install -y mysql-server-8.0
 
 sleep 5
 
