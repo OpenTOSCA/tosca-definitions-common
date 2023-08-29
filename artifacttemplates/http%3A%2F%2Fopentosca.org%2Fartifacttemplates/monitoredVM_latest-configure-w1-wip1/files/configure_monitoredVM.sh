eeb39334dbcc2ae6b62687b4417f0c7858d91b97
Maximilian Kuhn <maximilian.kuhn@ymail.com>
2023-08-29 14:34:21 +0200

    Fix issues with artifactTemplate

--- artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/monitoredVM_latest-configure-w1-wip1/files/configure_monitoredVM.sh
+++ artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/monitoredVM_latest-configure-w1-wip1/files/configure_monitoredVM.sh
@@ -6,17 +6,36 @@ apt-get install -y curl
 
 apt-get install -y cron
 
+name=$(hostname)
+
+cpu_name=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
+
+cpu_cores=$(grep -c processor /proc/cpuinfo)
+
+ram=$(free -m | awk '/Mem:/ { print $2 }')
+
+disk_size=$(df | awk '$NF=="/"{printf "%d", $2}')
+
+payload="{\"name\": $name, \"cpu_name\": $cpu_name, \"cpu_cores\": $cpu_cores, \"ram\": $ram, \"disk_size\": $disk_size}"
+
+curl -X POST -H "Content-Type: application/json" -d "$payload" "$QProvEndpoint"
+
 mkdir ~/monitoring
 
 cat > ~/monitoring/monitoring.sh<< EOF
 # Get CPU usage as a percentage
-cpu_usage=\$(top -bn 1 | grep "Cpu(s)" | awk '{print \$2 + \$4}')
+cpu_usage=\$[100-\$(vmstat 1 2|tail -1|awk '{print \$15}')]
+
+cpu_speed=\$(cat /proc/cpuinfo | grep 'cpu MHz' | cut -f 2 -d ":" | awk '{\$1=\$1}1' |head -1)
 
 # Get memory usage as a percentage
 mem_usage=\$(free | grep Mem | awk '{print (\$3 / \$2) * 100}')
 
+disk_usage=\$(df | awk '\$NF=="/"{printf "%d", \$5}')
+
+
 # Create a JSON payload
-payload="{\"cpu_usage\": \$cpu_usage, \"mem_usage\": \$mem_usage}"
+payload="{\"name\": $name, \"cpu_usage\": \$cpu_usage, \"cpu_speed\": \$cpu_speed, \"mem_usage\": \$mem_usage, \"disk_usage\": \$disk_usage}, \"timestamp\": \$(date +%s)}
 
 # Send data to the endpoint using cURL
 echo \$payload
